63aca6f6829529c25df7329272b51eacad3c47c7
Karim-Megahed <karim@amplerbikes.com>
2022-09-12 21:56:35 +0300

    env variables used

--- src/utils/sequelize.ts
+++ src/utils/sequelize.ts
@@ -1,8 +1,10 @@
 const { Sequelize } = require('sequelize')
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-const sequelize = new Sequelize('game', 'root', 'ampler25', {
-    host: 'localhost',
-    dialect: 'mysql'
-  });
 
-export default sequelize;
+
+export default new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+});
